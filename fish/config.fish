1dec09e4bf7d2930cebd7838501858df239c4d65
aliva <ali.vakilzade@gmail.com>
2013-05-17 15:31:43 +0430

    removed fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,6 @@ set -x HOME_BIN_PATH ~/bin
 alias s='sudo'
 alias yum='sudo yum'
 
-# I like the idea: http://www.reddit.com/tb/1d5m1c
-alias fucking='sudo'
-
 # python
 alias p2='python2'
 alias p3='python3'
