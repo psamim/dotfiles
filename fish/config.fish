82b0b6f54f959d434533c78b14cc7282cd64fc00
aliva <ali.vakilzade@gmail.com>
2013-05-25 00:54:13 +0430

    axel added to night job

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
-# fish path
+# fish path {{{
 set -x FISH ~/.config/fish
-
-# binary files are here
+# }}}
+# binary files are here {{{
 set -x PATH ~/Workspace/dotfiles/bin
 set -x PATH $PATH ~/Workspace/dotfiles/bin/local
 set -x PATH $PATH /usr/local/bin
@@ -9,13 +9,16 @@ set -x PATH $PATH /usr/bin
 set -x PATH $PATH /bin
 set -x PATH $PATH /usr/sbin
 set -x PATH $PATH /sbin
-
+# }}}
+# bin path in home {{{
 set -x HOME_BIN_PATH ~/bin
+# }}}
 
 # sudo
 alias s='sudo'
 alias yum='sudo yum'
-
 # python
 alias p2='python2'
 alias p3='python3'
+# wget
+alias wget='wget -c'
