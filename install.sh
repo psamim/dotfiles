a49c0032c346acd047823f38bc2544d95afd9a11
Samim Pezeshki <p.samim@gmail.com>
2013-07-31 23:03:43 +0430

    Install script was changed to use PWD"

--- install.sh
+++ install.sh
@@ -4,30 +4,33 @@
 # cron jobs are not included.
 
 # aria2/
-ln -sf "/home/$USER/dotfiles/aria2/aria2.conf" "/home/$USER/.aria2/.aria2.conf"
+mkdir -p "/home/$USER/.aria2/"
+ln -sf "$PWD/aria2/aria2.conf" "/home/$USER/.aria2/.aria2.conf"
 
 # bash/
-ln -sf "/home/$USER/dotfiles/bash/bashrc" "/home/$USER/.bashrc"
+ln -sf "$PWD/bash/bashrc" "/home/$USER/.bashrc"
 
 # bin/
-ln -sf "/home/$USER/dotfiles/bin" "/home/$USER/.local/bin"
+ln -sf "$PWD/bin" "/home/$USER/.local/bin"
 
 # fish/
-ln -sf "/home/$USER/dotfiles/fish" "/home/$USER/.config/fish/"
+mkdir -p "/home/$USER/.config/"
+ln -sf "$PWD/fish" "/home/$USER/.config/fish"
 
 # git/
-ln -sf "/home/$USER/dotfiles/git/config" "/home/$USER/.gitconfig"
-ln -sf "/home/$USER/dotfiles/git/ignore" "/home/$USER/.gitignore"
+ln -sf "$PWD/git/config" "/home/$USER/.gitconfig"
+ln -sf "$PWD/git/ignore" "/home/$USER/.gitignore"
 git config --global core.excludesfile '~/.gitignore'
 
 # hg/
-ln -sf "/home/$USER/dotfiles/hg/hgrc" "/home/$USER/.hg"
+ln -sf "$PWD/hg/hgrc" "/home/$USER/.hg"
 
 # komodo/
-ln -sf "/home/$USER/dotfiles/komodo" "/home/$USER/.komodoedit/8.0"
+mkdir -p "/home/$USER/.komodoedit/8.0"
+ln -sf "$PWD/komodo" "/home/$USER/.komodoedit/8.0"
 
 # tmux/
-ln -sf "/home/$USER/dotfiles/tmux/tmux.conf" "/home/$USER/.tmux.conf"
+ln -sf "$PWD/tmux/tmux.conf" "/home/$USER/.tmux.conf"
 
 # vim/
-ln -sf "/home/$USER/dotfiles/vim" "/home/$USER/.vim"
+ln -sf "$PWD/vim" "/home/$USER/.vim"
