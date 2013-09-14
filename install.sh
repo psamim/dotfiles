10823b3b3a4e643483c66f120642aa069a69b204
Samim Pezeshki <p.samim@gmail.com>
2013-09-15 00:05:21 +0430

    Many changes, esp Zsh

--- install.sh
+++ install.sh
@@ -42,6 +42,12 @@ ln -sf "$PWD/vim/vimrc" "/home/$USER/.vimrc"
 # Zsh/
 rm -rf "/home/$USER/.zsh"
 ln -sf "$PWD/zsh" "/home/$USER/.zsh"
+ln -sf "$PWD/zsh/prezto" "/home/$USER/.zprezto"
 rm -f "/home/$USER/.zshrc"
 ln -sf "$PWD/zsh/zshrc" "/home/$USER/.zshrc"
-
+ln -sf "$PWD/zsh/prezto/runcoms/zlogin" "/home/$USER/.zlogin"
+ln -sf "$PWD/zsh/prezto/runcoms/zlogout" "/home/$USER/.zlogout"
+ln -sf "$PWD/zsh/prezto/runcoms/zpreztorc" "/home/$USER/.zpreztorc"
+ln -sf "$PWD/zsh/prezto/runcoms/zprofile" "/home/$USER/.zprofile"
+ln -sf "$PWD/zsh/prezto/runcoms/zshenv" "/home/$USER/.zshenv"
+ln -sf "$PWD/zsh/prezto/runcoms/zshrc" "/home/$USER/.zshrc"
