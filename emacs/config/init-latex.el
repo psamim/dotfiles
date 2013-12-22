d80303a223829fa4bfb0180cc79e7db8d2adc4de
Samim Pezeshki <p.samim@gmail.com>
2013-12-23 01:03:04 +0330

    reftex for auctex

--- emacs/config/init-latex.el
+++ emacs/config/init-latex.el
@@ -1,5 +1,9 @@
 (require-package 'auctex)
 ;(require 'auctex-pkg)
 (require 'auctex-autoloads)
+(require 'reftex)
+
+(setq reftex-plug-into-AUCTeX t)
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
 (provide 'init-latex)
