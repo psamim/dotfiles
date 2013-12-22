15bbc632330b9c983175ff1ea814bf28fb864c71
Samim Pezeshki <p.samim@gmail.com>
2013-12-22 13:08:15 +0330

    autocomplete mode improvements

--- emacs/config/init-auto-complete.el
+++ emacs/config/init-auto-complete.el
@@ -15,6 +15,19 @@
   (add-to-list 'ac-modes mode))
 
 (ac-config-default)
+(setq-default ac-sources
+          '(
+        ac-source-filename
+        ac-source-abbrev 
+        ac-source-dictionary
+        ac-source-words-in-buffer
+        ac-source-yasnippet
+        ac-source-words-in-same-mode-buffers))
+
+; Latex Configs
+(defun my-ac-tex-setup()
+  (setq ac-sources (append '(ac-source-auctex-bibs) ac-sources)))
+(add-hook 'TeX-mode-hook 'my-ac-tex-setup)
 
 (after 'linum
   (ac-linum-workaround))
