816d7d9951cf0382e92a91cb0237c7d19355ed9c
Samim Pezeshki <p.samim@gmail.com>
2013-12-26 19:28:59 +0330

    Added autocompletion for strings

--- emacs/config/init-auto-complete.el
+++ emacs/config/init-auto-complete.el
@@ -2,6 +2,10 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
 
+; This adds the dabbrev to ac, for autocompleting strings, but did not work as intended for me
+;(require-package 'ac-dabbrev)
+;(require 'ac-dabbrev)
+
 (setq ac-auto-show-menu t)
 (setq ac-auto-start t)
 (setq ac-comphist-file (concat user-emacs-directory ".cache/ac-comphist.dat"))
@@ -18,23 +22,27 @@
 (setq-default ac-sources
           '(
         ac-source-filename
-        ac-source-abbrev 
+        ;ac-source-abbrev
         ac-source-dictionary
+        ;ac-source-dabbrev
         ac-source-words-in-buffer
-        ac-source-yasnippet
+        ;ac-source-yasnippet
         ac-source-words-in-same-mode-buffers))
 
 ; Latex Configs
-(defun my-ac-tex-setup()
-  (setq ac-sources (append '(
-                             ) ac-sources)))
+;(defun my-ac-tex-setup()
+ ; (setq ac-sources (append '(
+  ;                           ) ac-sources)))
 ;(add-hook 'LaTeX-mode-hook 'my-ac-tex-setup)
 
 (after 'linum
   (ac-linum-workaround))
 
-(defadvice ac-expand (before advice-for-ac-expand activate)
-  (when (yas-expand)
-    (ac-stop)))
+;(defadvice ac-expand (before advice-for-ac-expand activate)
+;  (when (yas-expand)
+;   (ac-stop)))
+
+; Auto-complete for everything even inside strings
+(setq ac-disable-faces nil)
 
 (provide 'init-auto-complete)
