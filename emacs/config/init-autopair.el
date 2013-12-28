26d2d8a58c5754ea51810ae79b71cdef4bb1e933
Samim Pezeshki <p.samim@gmail.com>
2013-12-28 23:16:28 +0330

    Pairing paranthesis

--- emacs/config/init-autopair.el
+++ emacs/config/init-autopair.el
@@ -2,4 +2,10 @@
 (require 'autopair)
 (autopair-global-mode)
 
+(electric-pair-mode)
+(show-paren-mode)
+
+(setq show-paren-delay 0)           ; how long to wait?
+(show-paren-mode t)                 ; turn paren-mode on
+(setq show-paren-style 'mixed) ; alternatives are 'parenthesis' and 'mixed'
 (provide 'init-autopair)
