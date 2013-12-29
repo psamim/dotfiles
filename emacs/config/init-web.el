003c42f20aa1f0ce29f10a3982be8347d46642e7
Samim Pezeshki <p.samim@gmail.com>
2013-12-30 01:51:28 +0330

    Skewer mode support added

--- emacs/config/init-web.el
+++ emacs/config/init-web.el
@@ -1,11 +1,13 @@
-;(require-package 'js2-mode)
-;(require 'js2-mode)
-;(setq js2-highlight-level 3)
-;(setq-default js2-basic-offset 2)
+(require-package 'js2-mode)
+(require 'js2-mode)
+(setq js2-highlight-level 3)
+(setq-default js2-basic-offset 2)
 
-;(require-package 'ac-js2)
-;(after 'js2-mode-autoloads
-  ;(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+(require-package 'ac-js2)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+
+(after 'js2-mode-autoloads
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
 
 ;(require-package 'js2-refactor)
 ;(require 'js2-refactor)
@@ -48,11 +50,14 @@
   ;(my-stylus-compile (point-min) (point-max)))
 
 
-;(require-package 'skewer-mode)
-;(skewer-setup)
+(require-package 'skewer-mode)
+(skewer-setup)
 
 (require-package 'php-mode)
 (require 'php-mode)
+
+(require-package 'php-eldoc)
+(add-hook 'php-mode-hook 'php-eldoc-enable)
 
 (require-package 'rainbow-mode)
 (require 'rainbow-mode)
