b6081525185f4ed58fcfdc8605acdedfc056b429
Samim Pezeshki <p.samim@gmail.com>
2014-01-04 09:49:24 +0330

    Some face customization

--- emacs/config/init-eyecandy.el
+++ emacs/config/init-eyecandy.el
@@ -48,7 +48,12 @@
 (load-theme 'solarized-dark)
 ;(load-theme 'sanityinc-tomorrow-night)
 
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(secondary-selection ((t (:background "#002B36")))))
 (require-package 'writeroom-mode)
 
 ;(require-package 'powerline)
