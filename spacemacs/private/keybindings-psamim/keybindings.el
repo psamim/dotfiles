217aa93ff3d978a50120a4e00916947ce312194f
Samim Pezeshki <p.samim@gmail.com>
2016-02-08 12:29:43 +0330

    Add psamim-keybindings spacesmacs layer

---
+++ spacemacs/private/keybindings-psamim/keybindings.el
@@ -0,0 +1,3 @@
+(spacemacs/set-leader-keys "d" 'ranger)
+(define-key evil-normal-state-map "Q" 'kill-this-buffer)
+(define-key evil-normal-state-map "W" 'save-buffer)
--- spacemacs/private/keybindings-psamim/packages.el
+++
@@ -1,9 +0,0 @@
-(setq keybindings-psamim-packages
-      '(ranger
-        ))
-
-(defun keybindings-psamim/post-init-ranger ()
-  (progn
-    (spacemacs/set-leader-keys
-      "d" 'ranger)
-    ))
