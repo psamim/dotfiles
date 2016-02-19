9ba3cd6c68e800c7a24829b4cf3ef78bf6773d0d
Samim Pezeshki <p.samim@gmail.com>
2016-02-19 11:16:00 +0330

    Use deer instead of ranger in emacs

--- spacemacs/private/psamim-defaults/keybindings.el
+++ spacemacs/private/psamim-defaults/keybindings.el
@@ -1,3 +1,3 @@
-(spacemacs/set-leader-keys "d" 'ranger)
+(spacemacs/set-leader-keys "d" 'deer)
 (define-key evil-normal-state-map "Q" 'kill-this-buffer)
 (define-key evil-normal-state-map "W" 'save-buffer)
