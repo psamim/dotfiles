8159b498b3a2bfa1010031a0ecd6c83599879748
Samim Pezeshki <p.samim@gmail.com>
2016-03-20 12:53:46 +0330

    Clean up bin directory, add doc

--- bin/download.sh
+++
@@ -1,3 +0,0 @@
-#echo "samim" >> /home/samim/whh
-/bin/aria2c --conf-path="$HOME/.aria2/aria2.conf" -i "$HOME/Downloads/nightly/downloads.txt" -d "$HOME/Downloads/nightly/" -l "$HOME/Downloads/nightly/log"
-
