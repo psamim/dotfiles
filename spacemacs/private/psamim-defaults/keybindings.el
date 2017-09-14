23a6b6505d999824f7d6050abb968e964f4b076d
Samim Pezeshki <p.samim@gmail.com>
2017-09-14 18:25:05 +0430

    Update config

--- spacemacs/private/psamim-defaults/keybindings.el
+++ spacemacs/private/psamim-defaults/keybindings.el
@@ -1,2 +1,3 @@
 (spacemacs/set-leader-keys "d" 'deer)
+(spacemacs/set-leader-keys "o" 'helm-mini)
 (define-key evil-normal-state-map "W" 'save-buffer)
--- spacemacs/private/psamim-defaults/packages.el
+++ spacemacs/private/psamim-defaults/packages.el
@@ -5,6 +5,8 @@
 (defun psamim-defaults/init-ranger ()
   (use-package ranger
     :config (progn
+              (ranger-override-dired-mode t)
+              (setq ranger-footer-delay 0.7)
               (define-key ranger-mode-map (kbd "M-n") 'ranger-new-tab)
               (define-key ranger-mode-map (kbd "M-l") 'ranger-next-tab)
               (define-key ranger-mode-map (kbd "M-h") 'ranger-prev-tab))))
