b29f3d0780df7b7fd5a8864bca7268649d4cbd20
Samim Pezeshki <psamim@gmail.com>
2019-01-22 09:24:15 +0330

    Update

--- spacemacs/private/psamim-org/packages.el
+++ spacemacs/private/psamim-org/packages.el
@@ -14,7 +14,7 @@
           org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
                                         ("STYLE_ALL" . "habit")))
           org-plantuml-jar-path (expand-file-name "~/Downloads/plantuml.jar")
-          org-export-babel-evaluate nil
+          ;; org-export-babel-evaluate nil
           org-confirm-babel-evaluate nil
           org-agenda-files (quote ("~/Notes/todo.org" "~/Notes/appointments.org"))
           org-directory "~/Notes"
