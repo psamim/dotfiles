d7d0c6be2ea488aeedeac72beb954fa865cda98b
Samim Pezeshki <psamim@gmail.com>
2019-10-16 16:31:02 +0330

    Update emacs keybindings

--- spacemacs/private/psamim-defaults/keybindings.el
+++ spacemacs/private/psamim-defaults/keybindings.el
@@ -1,3 +1,4 @@
 (spacemacs/set-leader-keys "d" 'deer)
 (spacemacs/set-leader-keys "o" 'helm-mini)
 (define-key evil-normal-state-map "W" 'save-buffer)
+(global-set-key (kbd "M-q") 'spacemacs/kill-this-buffer )
