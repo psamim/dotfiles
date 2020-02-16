0b5f37614bc3257042e889746ce8b2c2251fc23e
Samim Pezeshki <psamim@gmail.com>
2020-02-16 08:32:42 +0330

    Fix udev script

--- bin/keyboard_configure.sh
+++ bin/keyboard_configure.sh
@@ -1,3 +1,10 @@
+#! /bin/sh
 # Keyboard layout
+
+# Also have this under /etc/udev/rules.d/60-usb-keyboard.rules
+# ACTION=="add", ATTRS{phys}=="usb-0000:00:14.0-3/input0", RUN="/home/samim/.bin/keyboard_configure.sh" 
+
+export DISPLAY=:0
+export XAUTHORITY=/home/samim/.Xauthority 
 setxkbmap -layout us,ir -option 'grp:shift_toggle'
 setxkbmap -option caps:ctrl_modifier
