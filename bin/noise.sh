17fc6f2926ef3cb58080382728eab64e46b5cbc2
Samim Pezeshki <psamim@gmail.com>
2021-11-26 23:49:08 +0330

    Update noise.sh

--- bin/noise.sh
+++ bin/noise.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-#              bash 4.1.5(1)     Linux Ubuntu 10.04           Date : 2011-10-04
+#              bash 4.1.5(1)     Linux Ubuntu 10.04           Date : 2011-09-13
 #
 # _______________|  noise : ambient Brown noise generator (cf. white noise).
 #
 #           Usage:  noise  [minutes=59]  [band-pass freq center=1786]  [wave]
 #                          ^minutes can be any positive integer.
-#                           Command "noise 1" will display peak-level meter.
 #
 #    Dependencies:  play (from sox package)
 
@@ -17,17 +16,14 @@
 #  heavy rainfall. We shall filter it through a band-pass, then add effects 
 #  to mellow the output for ambience.
 
-#  Volume oscillation (amplitude modulation) is used to simulate artificially 
-#  long ocean waves. Try some of the free online services listed at the end 
-#  of this file to hear other types of oscillation.
-#
-#        Our goal is to block out distractions for calm concentration.
+#  [/] - could increase volume oscillation (amplitude modulation) to simulate 
+#           waves or breathing pattern -- but this could be too relaxing.
+#           Hear the free online services listed at the end of file.
+#           Our goal is to block out distractions for calm concentration.
 
 
-#  CHANGE LOG  get LATEST version from https://bitbucket.org/rsvp/gists/src
+#  CHANGE LOG  Code posted at https://gist.github.com/1209835
 #
-#  2011-09-14  Add volume amplification to compensate for effects previously 
-#                 applied. Use peak-level meter to avoid clipping.
 #  2011-09-13  Add bass and treble tone controls.
 #                 at loud volumes, low frequency thumping may be annoying, 
 #                 so reduce gain on bass. Treble can be used to reduce 
@@ -39,12 +35,11 @@
 #                 Fix tremolo to give very slow wave oscillation in volume.
 #                 (Thanks to xguse for his gist at github.)
 #                 Constant volume introduces tension psychologically.
-#                 Code posted at https://gist.github.com/1209835
 #  2011-09-10  First version based on 2009 article by Tom Swiss, and
 #                 subsequent comments. See below for relevant portions.
 
 #           _____ Prelims
-# set -u
+set -u
 #   ^ unbound (i.e. unassigned) variables shall be errors.
 #           Example of default assignment:    arg1=${1:-'foo'}
 set -e
@@ -52,11 +47,11 @@ set -e
 #
 # _______________     ::  BEGIN  Script ::::::::::::::::::::::::::::::::::::::::
 
-minutes='60'
+minutes=${1:-'59'}
 repeats=$(( minutes - 1 ))
-center='1786'
+center=${2:-'1786'}
 
-wave='0.0333333'
+wave=${3:-'0.0333333'}
 #         ^increase for more volume oscillation, but suggest no higher than 0.20
 #          (and no lower than 0.0166667). Its value should consider the 60
 #          seconds duration of the repeated sample.
@@ -66,8 +61,6 @@ noise='brown'
 #     where tpdf stands for Triangular Probability Density Function (cf. dither).
 #     N.B. - white and pink noise have higher frequencies than Brown.
 
-volume='1'
-
 len='01:00'
 #   ^CONSTANT one minute. (Format for specifying time length is hh:mm:ss.frac) 
 #     ___ATTN___ We first pre-compute one minute of audio output to file, 
@@ -75,70 +68,13 @@ len='01:00'
 #     This dramatically cuts CPU usage by 95% after the first minute.
 
 
-#  For DEBUGGING: "noise 1" shows the peak-level meter; also instant production.
-if [ $minutes -eq 1 ] ; then
-     progress='--show-progress'
-else
-     progress='--no-show-progress'
-fi
+echo " ::  Please stand-by... sox will 'play' $noise noise for $minutes minutes."
 
 
-HELPTEXT="
-	minutes		length of time to play the noise
-	
-	center		Apply a band-pass filter.  The frequency response drops
-			logarithmically around the center frequency.  The width
-			parameter gives the slope of the drop.  The frequencies at
-			center + width and center - width will be half of their
-			original amplitudes.  band defaults to a mode oriented to
-			pitched audio, i.e. voice, singing, or instrumental music.
-			The -n (for noise) option uses the alternate mode for un-
-			pitched audio (e.g. percussion).  Warning: -n introduces a
-			power-gain of about 11dB in the filter, so beware of output
-			clipping.  band introduces noise in the shape of the filter,
-			i.e. peaking at the center frequency and settling around it
-	
-	wave		increase for more volume oscillation, but suggest no higher than 0.20
-			(and no lower than 0.0166667). Its value should consider the 60
-			seconds duration of the repeated sample.
-	
-	noise		your choice: 'white', 'pink', 'brown', 'tpdf'
-			where tpdf stands for Triangular Probability Density Function (cf. dither).
-			N.B. - white and pink noise have higher frequencies than Brown.
-
-	volume		less than 0 decreases the volume, more than 0 increases it. Be careful: 
-			increasing this may cause clipping.
-"
-
-while getopts 'm:c:w:n:v:' OPTION
-	do
-		case $OPTION in
-			m)	minutes="$OPTARG"
-				;;
-			c)	center="$OPTARG"
-				;;
-			w)	wave="$OPTARG"
-				;;
-			n)	noise="$OPTARG"
-				;;
-			v)	volume="$OPTARG"
-				;;
-			?)	printf "Usage: %s: [-m minutes <$minutes>] [-c center <$center>] [-w wave <$wave>] [-n noise <$noise>] [-v volume <$volume>]\n${HELPTEXT}" $(basename $0)
-				exit 2
-				;;
-		esac
-	done
-
-echo " ::  Please stand-by... sox will 'play' $noise noise for $minutes minute(s)."
-#          FYI  Channels: 2 @ 32-bit,  Samplerate: 48000Hz.
-
-
-play $progress  -c 2  --null  synth  $len  ${noise}noise  \
+play --no-show-progress  -c 2  --null  synth  $len  ${noise}noise  \
      band -n $center 499               \
      tremolo $wave    43   reverb 19   \
-     bass -11              treble -1   \
-     vol     14dB                      \
-     vol     $volume                   \
+     bass -9               treble +1   \
      repeat  $repeats
 
      
@@ -147,6 +83,9 @@ play $progress  -c 2  --null  synth  $len  ${noise}noise  \
 #  play -c 2 --null synth $len brownnoise band -n 2500 4000 tremolo 20 .1 reverb 50
 #  play --null synth $len brownnoise band -n 1200 200 tremolo 20 .1 reverb 20
 #  play --null synth $len brownnoise band -n 1200 200 tremolo 20 .1
+
+#  #  Q:  Using rate "-r 44100" produces overblown output -- why?
+#  #      (even with --guard against clipping ;-)
 
 
 #            _____ ARGUMENTS explained via "man sox"
@@ -240,21 +179,6 @@ play $progress  -c 2  --null  synth  $len  ${noise}noise  \
 #           case of brownnoise with effects. Reduce annoyance accordingly.
 
 
-#       vol gain 
-#           Apply an amplification or an attenuation to the audio signal.
-#           Unlike the -v option (which is used for balancing multiple
-#           input files as they enter the SoX effects processing chain),
-#           vol is an effect like any other so can be applied anywhere,
-#           and several times if necessary, during the processing chain.
-#  
-#           The amount to change the volume is given by gain which is
-#           interpreted, according to the given type, as follows: 
-#           if dB, then a power change in dB.  When type is dB, a gain 
-#           of 0 leaves the volume unchanged, less than 0 decreases it, 
-#           and greater than 0 increases it.
-#           Beware of clipping when the increasing the volume.
-
-
 #       repeat count
 #           Repeat the entire audio count times.  Requires temporary file
 #           space to store the audio to be repeated. [But where exactly?] 
@@ -309,18 +233,11 @@ exit 0
 #           _____ Free ONLINE alternatives
 #
 #  Simply Noise for white, pink and brown/red noise generator; uses Flash:
-#       http://simplynoise.com   (App is $0.99) 
-#
-#       [Flash consumes about 30 times more than our script in CPU usage!]
+#       http://simplynoise.com   $0.99 app available
+#  [Flash consumes about 30 times more than our script in CPU usage!]
 #
 #  PlayNoise for white, pink, and brown noise generator; uses Javascript/HTML5:
 #       http://playnoise.com
-#
-#  Random.org has 33-second sample audio files containing perfect white noise. 
-#       The randomness comes from atmospheric noise, which is more natural 
-#       than the pseudo-random number algorithms. Such files could serve as 
-#       input to sox for further signal processing.
-#       http://www.random.org/audio-noise/
 
 
 #  _______________ HOW WHITE NOISE WORKS by Saabira Chaudhuri
@@ -353,8 +270,6 @@ exit 0
 #  the makers of downloadable apps and sleep machines use the term anyway.  White
 #  noise and other soothing sounds, once mainly played on machines to aid
 #  nighttime sleep, are increasingly helping make daytime hours more serene.
-#  [White noise is a common synthetic noise source used for sound masking by 
-#  a tinnitus masker.]
 #  
 #  After HeavyDutyApps released an app called Sleep Pillow Ambiance to help
 #  people sleep, it quickly realized that many customers used it during the day
@@ -379,13 +294,6 @@ exit 0
 #  not sleep without listening to it and that she had burned [out] six hair
 #  dryers over the years."
 #  
-#  Daytime white-noise listeners say the sounds serve two main purposes: to block
-#  out distractions and lessen sounds that cause anxiety, such as sirens.
-#  "Certain types of noises can be relaxing," says Robert C. Fifer, director of
-#  audiology and speech language pathology at the University of Miami. White
-#  noise can be used to create a more relaxing working environment, masking
-#  sounds and promoting a sense of privacy, he says.
-#  
 #  One small study examined white noise in a classroom environment. The research,
 #  led by Goran Soderlund and Sverker Sikström of Stockholm University, looked at
 #  51 students at a secondary school in Norway and found that those who normally
@@ -397,16 +305,23 @@ exit 0
 #  brain work more efficiently. The study predicted that white noise could help
 #  children with attention deficit hyperactivity disorder (ADHD) learn to focus
 #  on schoolwork better.
+#  
+#  Daytime white-noise listeners say the sounds serve two main purposes: to block
+#  out distractions and lessen sounds that cause anxiety, such as sirens.
+#  "Certain types of noises can be relaxing," says Robert C. Fifer, director of
+#  audiology and speech language pathology at the University of Miami. White
+#  noise can be used to create a more relaxing working environment, masking
+#  sounds and promoting a sense of privacy, he says.
 
-#           _____ REFERENCES
+
+#           _____ Other REFERENCES
 #
+#  Re: Brown noise, see http://en.wikipedia.org/wiki/Brownian_noise
+
 #  "The effects of background white noise on memory performance 
 #       in inattentive school children"
 #  Göran BW Söderlund1, Sverker Sikström, Jan M Loftesnes and EJ Sonuga-Barke
 #  Behavioral and Brain Functions 2010, 6:55 doi:10.1186/1744-9081-6-55
 #  Published: 29 September 2010 
 #  http://www.behavioralandbrainfunctions.com/content/6/1/55/abstract
-
-
-#  Re: Brown noise, see http://en.wikipedia.org/wiki/Brownian_noise
 
