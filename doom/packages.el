8b51a23069f31f9343f79aa12b92568b405d07d2
Samim Pezeshki <psamim@gmail.com>
2024-01-16 18:43:52 +0100

    Update Emacs config

--- doom/packages.el
+++ doom/packages.el
@@ -11,44 +11,44 @@
 
 ;; All of Doom's packages are pinned to a specific commit, and updated from
 ;; release to release. To un-pin all packages and live on the edge, do:
-;(unpin! t)
+;; (unpin! t)
 
 ;; ...but to unpin a single package:
-;(unpin! pinned-package)
+;; (unpin! pinned-package)
 ;; Use it to unpin multiple packages
-;(unpin! pinned-package another-pinned-package)
+;; (unpin! pinned-package another-pinned-package)
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+;; (package! some-package)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+;; (package! another-package
+;;  :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+;; (package! this-package
+;;  :recipe (:host github :repo "username/repo"
+;;           :files ("some-file.el" "src/lisp/*.el")))
 
 ;; If you'd like to disable a package included with Doom, for whatever reason,
 ;; you can do so here with the `:disable' property:
-;(package! builtin-package :disable t)
+;; (package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
 ;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+;; (package! builtin-package :recipe (:nonrecursive t))
+;; (package! builtin-package-2 :recipe (:repo "myfork/package"))
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+;; (package! builtin-package :recipe (:branch "develop"))
 
 ;; (package! org-clock-csv)
 ;; (package! org-ql)
@@ -73,8 +73,8 @@
 ;; (package! counsel-org-clock)
 (package! graphviz-dot-mode)
 ;; (package! org-ql)
-; (package! org-super-agenda)
-(package! org-mind-map)
+;; (package! org-super-agenda)
+;; (package! org-mind-map)
 (package! solaire-mode :disable t)
 (package! websocket)
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
@@ -83,15 +83,15 @@
 ;; (package! org-analyzer)
 ;; (package! org-inline-pdf)
 (package! org-appear :recipe (:host github :repo "awth13/org-appear"))
-; (package! org-modern :recipe (:host github :repo "minad/org-modern"))
-; (package! org-xournalpp
-;   :recipe (:host gitlab
-;            :repo "vherrmann/org-xournalpp"
-;            :files ("resources" "*.el")))
-; (package! org-krita
-;    :recipe (:host github
-;            :repo "lepisma/org-krita"
-;            :files ("resources" "resources" "*.el" "*.el")))
+;; (package! org-modern :recipe (:host github :repo "minad/org-modern"))
+;; (package! org-xournalpp
+;;   :recipe (:host gitlab
+;;            :repo "vherrmann/org-xournalpp"
+;;            :files ("resources" "*.el")))
+;; (package! org-krita
+;;    :recipe (:host github
+;;            :repo "lepisma/org-krita"
+;;            :files ("resources" "resources" "*.el" "*.el")))
 
 ;; (package! ob-ledger
 ;;   :recipe (:host github :repo "overtone/emacs-live"
