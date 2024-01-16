fbf7c678f2e3450d4f4173963b8f53b192f05505
Samim Pezeshki <psamim@gmail.com>
2024-01-16 18:38:58 +0100

    Update configs

--- doom/config.el
+++ doom/config.el
@@ -33,7 +33,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-solarized-light)
 
 (setq org-agenda-clock-report-header "Report\n")
 
