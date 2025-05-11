47b6210a1ffd743cf3d54e4e81f02f6df691fb71
Samim Pezeshki <psamim@gmail.com>
2025-05-11 23:48:25 +0200

    Update vim mappings

---
+++ astronvim/lua/plugins/mappings.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          ["<Leader>fs"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- condition for only server with declaration capabilities
+          gD = {
+            function() vim.lsp.buf.declaration() end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
