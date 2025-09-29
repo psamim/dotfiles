d1ebfa28d8d06bc06739cd11e4763d9371692265
Samim Pezeshki <psamim@gmail.com>
2025-09-29 15:23:49 +0200

    Add graphql to treestitter

--- astronvim/lua/plugins/treesitter.lua
+++ astronvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,6 +7,7 @@ return {
     ensure_installed = {
       "lua",
       "vim",
+      "graphql",
       -- add more arguments for adding more treesitter parsers
     },
   },
