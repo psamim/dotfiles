29561c6c279421ea6009ce4299a3312a462b1bbe
Samim Pezeshki <psamim@gmail.com>
2026-09-03 12:08:59 +0200

    Add ghostty

--- install.sh
+++ install.sh
@@ -95,6 +95,13 @@ mkdir -p $HOME/.config/kitty
 ln -sf $PWD/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 ln -sf $PWD/kitty/quick-access-terminal.conf $HOME/.config/kitty/quick-access-terminal.conf
 
+# ghostty
+mkdir -p $HOME/.config/ghostty
+ln -sf $PWD/ghostty/config $HOME/.config/ghostty/config
+if [ ! -d "$HOME/.config/ghostty/shaders" ]; then
+  git clone https://github.com/sahaj-b/ghostty-cursor-shaders "$HOME/.config/ghostty/shaders"
+fi
+
 # workmux
 mkdir -p "$HOME/.config/workmux"
 ln -sf "$PWD/workmux/config.yaml" "$HOME/.config/workmux/config.yaml"
